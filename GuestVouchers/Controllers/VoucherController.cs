47c883b5d89b3cff7074ef649d7d9a2b8b90be78
mogge182 <mogge182@hotmail.com>
2017-05-11 21:57:21 +0200

    First Commit

---
+++ GuestVouchers/Controllers/VoucherController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace GuestVouchers.Controllers
+{
+    public class VoucherController : Controller
+    {
+        // GET: Voucher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("voucher/getvoucher")]
+        public string GetVoucher()
+        {
+            XmlDocument xdoc = new XmlDocument();
+            FileStream rfile = new FileStream(Server.MapPath("~/App_Data/XmlDB.xml"), FileMode.Open);
+            xdoc.Load(rfile);
+            rfile.Close();
+            XmlNodeList list = xdoc.GetElementsByTagName("Voucher");
+            XmlElement cl;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                cl = (XmlElement)xdoc.GetElementsByTagName("Code")[i];
+                XmlElement add = (XmlElement)xdoc.GetElementsByTagName("Used")[i];
+                if (add.InnerText == "false")
+                {
+                    add.InnerText = true.ToString();
+                    xdoc.Save(Server.MapPath("~/App_Data/XmlDB.xml"));
+                    return cl.InnerText;
+                }
+            }
+            rfile.Close();
+
+            return "No voucher found";
+
+
+        }
+    }
+}
---
+++ GuestVouchers/Scripts/VoucherJavaScipt.js
@@ -0,0 +1,10 @@
+﻿function test() {
+    $.ajax({
+        url: 'http://vouchertest.azurewebsites.net/voucher/getvoucher'
+        //data: { id: id }
+    }).done(function (value) {
+        document.getElementById("Voucher").innerText = value;
+    });
+}
+
+//http://localhost:52546/voucher/getvoucher
